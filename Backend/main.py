c4d5f49bda05e0716a71851f6bb284543cc50f3b
Pooja <poojavarahachalam@gmail.com>
2026-05-26 15:13:20 +0530

    Initial commit

---
+++ Backend/main.py
@@ -0,0 +1,246 @@
+from fastapi import FastAPI, Body
+
+
+
+app = FastAPI()
+
+# -----------------------------
+# Dummy Database
+# -----------------------------
+
+Movies = [
+    {
+    "id": 1,
+    "movie_name": "Inception",
+    "genre": "Sci-Fi",
+    "language": "English",
+    "rating": 9
+},
+    {
+    "id": 2,
+    "movie_name": "The Godfather",
+    "genre": "Crime",
+    "language": "English",
+    "rating": 8.5
+},
+
+    {
+    "id": 3,
+    "movie_name": "Parasite",
+    "genre": "Drama",
+    "language": "Korean",
+    "rating": 8.6
+},
+    {
+    "id": 4,
+    "movie_name": "3 Idiots",
+    "genre": "Comedy",
+    "language": "Hindi",
+    "rating": 9.5
+},
+    {
+    "id": 5,
+    "movie_name": "Avengers: Endgame",
+    "genre": "Action",
+    "language": "English",
+    "rating": 8.4
+
+},
+
+   {
+    "id": 6,
+    "movie_name": "The Dark Knight",
+    "genre": "Action",
+    "language": "English",
+    "rating": 9.0
+},
+
+    {
+    "id": 7,  
+    "movie_name": "Interstellar",
+    "genre": "Sci-Fi",
+    "language": "English",
+    "rating": 8.6
+},
+
+    {
+    "id": 8,
+    "movie_name": "The Shawshank Redemption",
+    "genre": "Drama",
+    "language": "English",
+    "rating": 9.3
+
+},
+
+   {
+    "id": 9,
+    "movie_name": "Pulp Fiction",
+    "genre": "Crime",
+    "language": "English",
+    "rating": 8.9
+},
+
+    {
+    "id": 10,
+    "movie_name": "The Matrix",
+    "genre": "Sci-Fi",
+    "language": "English",
+    "rating": 8.7
+}]
+
+# -----------------------------
+# Home Route
+# -----------------------------
+
+
+@app.get("/")
+def home():
+
+
+    return {
+        "message": "Welcome to Movie Database API!"
+    }
+
+# -----------------------------
+# Get All Movies
+# -----------------------------
+
+@app.get("/movies")
+
+def get_movies():
+    return Movies
+
+
+# -----------------------------
+# Get single movie by id
+# -----------------------------
+
+@app.get("/movies/{movie_id}")
+def get_movie(movie_id: int):
+
+
+    for movie in Movies:
+
+
+        if movie["id"] == movie_id:
+
+
+            return {
+                "movie": movie
+            }
+
+
+    return {
+        "message": "Movie Not Found"
+    }
+
+# -----------------------------
+# filter movies 
+# -----------------------------
+
+
+
+@app.post("/searchmovies/filter")
+
+def search_movies(filters: dict = Body()):
+
+    genre = filters.get("genre")
+    language = filters.get("language")
+    rating = filters.get("rating")
+
+    filtered_movies = Movies
+
+
+    if genre:
+        filtered_movies = [movie for movie in filtered_movies if genre.lower() in movie["genre"].lower()]
+
+    if language:
+        filtered_movies = [movie for movie in filtered_movies if language.lower() in movie["language"].lower()]
+
+    if rating:
+        filtered_movies = [movie for movie in filtered_movies if float(movie["rating"]) >= float(rating)]
+
+    if not filtered_movies:
+        return {
+            "message": "Movie Not Found"
+        }
+
+    return {
+        "filtered_movies": filtered_movies
+    }
+
+
+
+# -----------------------------
+# Add Movie 
+# -----------------------------
+
+
+@app.post("/addmovie")
+def add_movie(movie: dict = Body()):
+
+
+    Movies.append(movie)
+
+
+    return {
+        "message": "Movie Added Successfully",
+        "movie": movie
+    }
+
+
+# -----------------------------
+# update Movie 
+# -----------------------------
+
+@app.put("/updatemovie/{movie_id}")
+def update_movie(movie_id: int, updated_data: dict = Body()):
+
+
+    for movie in Movies:
+
+
+        if movie["id"] == movie_id:
+
+
+            movie.update(updated_data)
+
+
+            return {
+                "message": "Movie Updated Successfully",
+                "updated_movie": movie
+            }
+
+
+    return {
+        "message": "Movie Not updated. Please try again!"
+    }
+
+
+# -----------------------------
+# delete Movie 
+# -----------------------------
+
+@app.delete("/deletemovie/{movie_id}")
+def delete_movie(movie_id: int):
+    for i in range(len(Movies)):
+        if Movies[i]["id"] == movie_id:
+            deleted_movie = Movies.pop(i)
+            return {
+                "success": True,
+                "message": "Movie Deleted Successfully",
+                "deleted_movie": deleted_movie
+            }
+
+    return {
+        "success": False,
+        "message": "Movie Not Found"
+    }
+
+
+
+
+
+
+
+
---
+++ Frontend/app.py
@@ -0,0 +1,232 @@
+import streamlit as st
+
+import requests
+
+
+API_URL = "http://127.0.0.1:8000"
+
+
+st.title("🎥🍿 Movie Explorer")
+st.write("Welcome to the Movie Explorer!")    
+
+
+
+# -----------------------------
+# Sidebar Menu
+# -----------------------------
+
+
+menu = st.sidebar.selectbox(
+    "Select Option",
+    [
+
+        "Choose an Option",
+        "View All Movies",
+        "Search or Filter Movies",
+        "Add New Movie",
+        "Update Movie",
+        "Delete Movie"
+    ]
+)
+
+
+if menu == "Choose an Option":
+    st.info("Please select an option from the sidebar.")
+
+# -----------------------------
+# View All Movies
+# -----------------------------
+
+elif menu == "View All Movies":
+    st.header("All Movies")
+    response = requests.get(f"{API_URL}/movies")
+    Movies = response.json()
+    
+
+    if Movies:
+        st.success(f"Successfully loaded {len(Movies)} movies!")
+        st.write("---")
+        for movie in Movies:
+            with st.container():
+                col1, col2, col3, col4 = st.columns([2, 1, 1, 1])
+                
+                
+                with col1:
+                    st.markdown(f"### 🎥 {movie.get('movie_name', 'N/A')}")
+
+                with col2:
+                    st.markdown(f"**Genre:**\n{movie.get('genre', 'N/A')}")
+
+                with col3:
+                    st.markdown(f"**Language:**\n{movie.get('language', 'N/A')}")
+
+                with col4:
+                    st.markdown(f"**Rating:**\n⭐ {movie.get('rating', 'N/A')}")
+
+                st.write("---")
+
+    else:
+        st.info("The database is currently empty. No movies found!")
+
+
+
+# -----------------------------
+# Search or Filter Movies
+# -----------------------------
+
+elif menu == "Search or Filter Movies":
+    st.header("Search or Filter Movies")
+
+    with st.form(key="search_filter_form"):
+        Genre = st.selectbox("Genre", ["Action", "Crime", "Comedy", "Drama", "Horror", "Sci-Fi"])
+        Language = st.selectbox("Language", ["English", "Hindi", "Telugu", "Korean"])
+        rating = st.slider("Rating", min_value=1, max_value=10)
+
+        submit_button = st.form_submit_button(label="Search Movies")
+
+    if submit_button:
+        search_criteria = {
+            "genre": Genre,
+            "language": Language,
+            "rating": rating
+        }
+
+
+        response = requests.post(
+            f"{API_URL}/searchmovies/filter",
+            json=search_criteria
+            )
+
+        if response.status_code == 200:
+            data = response.json()
+            
+            if "filtered_movies" in data:
+                
+                movies = data["filtered_movies"]
+                
+                st.success("Movies found successfully!")
+                
+                for movie in movies:
+                    with st.container():
+                        col1, col2, col3, col4 = st.columns([2,1,1,1])
+                        
+                        with col1:
+                            st.markdown(f"### 🎥 {movie.get('movie_name')}")
+
+                        with col2:
+                            st.markdown(f"**Genre:** {movie.get('genre')}")
+
+                        with col3:
+                            st.markdown(f"**Language:** {movie.get('language')}")
+
+                        with col4:
+                            st.markdown(f"⭐ {movie.get('rating')}")
+                            
+                        st.write("---")
+
+            else:
+                st.error("Movie Not Found")
+    
+
+
+
+# -----------------------------
+# adding a movie
+# -----------------------------
+
+
+
+elif menu == "Add New Movie":
+    st.header("Add a New Movie")
+    with st.form(key="add_movie_form"):
+        Movie_id = st.number_input("Movie ID", min_value=1, step=1)
+        Movie_name = st.text_input("Movie_name")
+        genre = st.selectbox("Genre", ["Action", "Crime", "Comedy", "Drama", "Horror", "Sci-Fi"])
+        language = st.selectbox("Language", ["English", "Hindi", "Telugu", "Korean"])
+        rating = st.slider("Rating", min_value=1, max_value=10)
+
+        submit_button = st.form_submit_button(label="Add Movie")
+
+    if submit_button:
+        new_movie = {
+            "id": Movie_id,
+            "movie_name": Movie_name,
+            "genre": genre,
+            "language": language,
+            "rating": rating
+        }
+
+
+        response = requests.post(
+            f"{API_URL}/addmovie",
+            json=new_movie
+        )
+
+        if response.status_code == 200:
+            st.success(f"Movie '{Movie_name}' added successfully!")
+        else:
+            st.error(f"Movie '{Movie_name}' Not added. Please try again!")
+
+
+# -----------------------------
+# updating a movie
+# -----------------------------
+
+elif menu == "Update Movie":
+    st.header("Update a Movie")
+    with st.form(key="update_movie_form"):
+        Movie_id = st.number_input("Movie ID", min_value=1, step=1)
+        Movie_name = st.text_input("Movie_name")
+        genre = st.selectbox("Genre", ["Action", "Crime", "Comedy", "Drama", "Horror", "Sci-Fi"])
+        language = st.selectbox("Language", ["English", "Hindi", "Telugu", "Korean"])
+        rating = st.slider("Rating", min_value=1, max_value=10)
+
+        submit_button = st.form_submit_button(label="Update Movie")
+
+    if submit_button:
+        Update_movie = {
+            "id": Movie_id,
+            "movie_name": Movie_name,
+            "genre": genre,
+            "language": language,
+            "rating": rating
+        }
+
+
+        response = requests.put(
+            f"{API_URL}/updatemovie/{Movie_id}",
+            json=Update_movie
+            )
+
+        if response.status_code == 200:
+            st.success(f"Movie '{Movie_name}' updated successfully!")
+        else:
+            st.error(f"Movie '{Movie_name}' Not updated. Please try again!")
+
+
+# -----------------------------
+# delete a movie
+# -----------------------------
+
+elif menu == "Delete Movie":
+    st.header("Delete a Movie")
+
+    with st.form(key="delete_movie_form"):
+        Movie_id = st.number_input("Movie ID", min_value=1, step=1)
+
+        submit_button = st.form_submit_button(label="Delete Movie")
+
+    if submit_button:
+        response = requests.delete(
+            f"{API_URL}/deletemovie/{Movie_id}"
+            )
+        
+        data = response.json()
+        
+        if data["success"]:
+            st.success(f"Movie id '{Movie_id}' deleted successfully!")
+
+        else:
+            st.error(data["message"])
+
+
